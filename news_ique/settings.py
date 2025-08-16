45e84e273b4a1b5f70967019cc119c8fe01709a0
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-08-16 15:19:18 +0600

    create custom user model

--- news_ique/settings.py
+++ news_ique/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL ='users.USER'
 
 # Application definition
 
@@ -77,13 +78,24 @@ WSGI_APPLICATION = 'news_ique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME',default=''),
+        'USER': config('DB_USER',default =''),
+        'PASSWORD': config('DB_PASSWORD',default=''),
+        'HOST': config('DB_HOST',default='localhost'),
+        'PORT': config('DB_PORT',cast=int)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('EDITOR','Editor'),
+        ('SUBSCRIBER','Subscriber')
+    ]
+
+    address = models.CharField(max_length=200,blank=True,null=True)
+    phone_number = models.CharField(max_length=15)
+    role = models.CharField(max_length=15,choices=ROLE_CHOICES,default='SUBSCRIBER')
+
+    def __str__(self):
+        return self.username
